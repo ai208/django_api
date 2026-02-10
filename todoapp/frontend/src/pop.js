9b53e8169ea10bec7486ef8f7d0ada98c6687fdc
ai208 <yasu6328hand@gmail.com>
2026-02-10 20:59:18 +0900

    ch13 react app

---
+++ todoapp/frontend/src/pop.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+
+
+function Pop() {
+  return (
+    <div className="Pop">
+      <header className="Pop-header">
+        練習
+      </header>
+      <h1>練習</h1>
+      <p>初アプリ</p>
+    </div>
+  );
+}
+
+
+export default Pop;
