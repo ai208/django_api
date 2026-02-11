280127f3ac4aa08b22ba36bad834db44be2c5c56
ai208 <yasu6328hand@gmail.com>
2026-02-12 00:52:26 +0900

    ch 14 fin

---
+++ todoapp/frontend/src/components/add-todo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function AddTodo(){
+    return(
+        <div className="App">
+            Add Todo
+        </div>
+    );
+}
+export default AddTodo;
--- todoapp/frontend/src/index.js
+++ todoapp/frontend/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Pop from './pop';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter><App /></BrowserRouter>
     <Pop />
   </React.StrictMode>
 );
